738dc53c5ec39a6f759bacc062c01e3a3fe15a52
Luis Cobo <luis.cobo@gmail.com>
2021-06-16 22:22:14 -0500

    Dahlia, versión junio. Asignarle imagen al evento y obtener la imagen del evento.

---
+++ app/Http/Controllers/Api/V1/ImageUploadController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Evento;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+
+class ImageUploadController extends Controller
+{
+    // Permite guardar una imagen de un evento desde el cliente
+    public function upload_event_image(Request $request)
+    {
+        // Primero validamos los elementos del requerimiento
+        $request->validate([
+            'id'    => 'required|exists:eventos,id',
+            'image' => 'required'
+        ]);
+
+        // Ahora obtenemos el archivo con la imagen y lo guardamos en la carpeta correspondiente
+        $event_id = $request->id;
+        $result = $request->file('image')->store('images/events');
+
+        // Ahora buscamos el evento que tiene el identificador dado
+        $evento = Evento::find($event_id);
+        $evento->imagen = $result;
+        $evento->save();
+
+        // Enviamos la respuesta de regreso al cliente
+        return response()->json([
+            'status' => 1,
+            'message' => 'Image uploded successfully',
+            'event_id' => $event_id
+        ]);
+    }
+
+    // Permite obtener el archivo de imagen asociado a un evento
+    public function download_event_image(Request $request)
+    {
+        // Primero validamos los elementos del requerimiento
+        $request->validate([
+            'id' => 'required|exists:eventos,id'
+        ]);
+
+        // Ahora traemos la ruta donde se encuentra la imagen del evento
+        $evento_id = $request->id;
+        $evento = Evento::findOrFail($evento_id);
+        $imagen = $evento->imagen;
+
+        // Si la imagen no existe, abortamos
+        if (!Storage::exists($imagen)) {
+            abort(404);
+        }
+
+        // Enviamos la imagen al cliente
+        $type = Storage::mimeType($imagen);
+        return Storage::response($imagen, "imagen", ['Content-Type' => $type]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\V1\EventController;
 use App\Http\Controllers\Api\V1\ContactController;
+use App\Http\Controllers\Api\V1\ImageUploadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -32,6 +33,12 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     // Para registrar un nuevo evento
     Route::post('v1/event/register', [EventController::class, 'register']);
+
+    // Para asignarle una imagen a un evento
+    Route::post('v1/event/set_image', [ImageUploadController::class, 'upload_event_image']);
+
+    // Para descargar la imagen asociada a un evento
+    Route::get('v1/event/get_image', [ImageUploadController::class, 'download_event_image']);
 
     // Para registrar un nuevo contacto en un evento
     Route::post('v1/contact/register', [ContactController::class, 'add_contact_to_event']);
